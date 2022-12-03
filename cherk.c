6aef91c2b8bcbc116cfa1f01c7f66ec0c71b1328
-iwody <superkurvaaa@gmail.com>
2022-12-03 10:48:18 +0700

    braindeadmonkey

---
+++ cherk.c
@@ -0,0 +1,6 @@
+#include <stdio>
+int main()
+{
+    int a = 0 scanf("%d", a)
+        a = 235 int sum = 0;
+    // a
---
+++ drochevo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 1;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    if (a > b)
+    {
+        printf("%d", a);
+    }
+    else
+    {
+        printf("%d", b);
+    }
+    return 0;
+}
