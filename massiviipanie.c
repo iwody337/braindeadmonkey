3257a09e53543f4820e228f37e93c164bcfca55f
-iwody <superkurvaaa@gmail.com>
2022-12-09 12:00:40 +0700

    braindeadmonkey

--- massiviipanie.c
+++ massiviipanie.c
@@ -27,5 +27,27 @@ void main(void)
 
         sum += A[i];
     }
-    printf("Сумма всех элементов = %d\n", sum);
+    {
+        int B[N] = {0};
+        int i = 0, sum1 = 0;
+        printf("Введите массив из %d чисел:\n", N);
+        for (i = 0; i < N; i++)
+        {
+            printf("A[%d]=", i);
+            scanf("%d", &A[i]);
+        }
+        printf("Введеный массив:\n");
+        for (i = 0; i < N; i++)
+        {
+            printf("A[%d]=%d\n", i, A[i]);
+        }
+        for (i = 0; i < N; i++)
+        {
+
+            sum += A[i], B[i];
+        }
+    }
+    {
+        printf("Сумма всех элементов = %d\n", sum);
+    }
 }
