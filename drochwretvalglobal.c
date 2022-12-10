dc4423e613670a23f26278f21bceb06010872c35
iwody <superkurvaaa@gmail.com>
2022-12-10 11:13:58 +0700

    braindeadmonkey

--- drochwretvalglobal.c
+++ drochwretvalglobal.c
@@ -26,3 +26,48 @@ void printArray(int*array, int size){
         printf("array[%d]=%d\n",i,array[i]);
     }
 }
+
+
+
+
+
+
+ #include <stdio.h>
+ #include <time.h>
+#include <stdlib.h>
+#define SIZE 3
+void fillRandom(int *array, int size);
+void fill(int *array, int size);
+void printArray(int*array, int size);
+int main(void){
+    int massOne[SIZE]={0};
+    //fill(massOne, SIZE);
+    fillRandom(massOne, SIZE);
+    printArray(massOne, SIZE);
+    int *massTwo=(int*)calloc(SIZE, sizeof(int));
+    //fill(massTwo, SIZE);
+    fillRandom(massTwo, SIZE);
+    printArray(massTwo, SIZE);
+    
+free(massTwo);
+exit(EXIT_SUCCESS);
+}
+void fill(int *array, int size){
+    printf("Введи %d элементов: \n", size);
+    for (int i=0; i<size; ++i){
+        printf("array[%d]= ", i);
+        scanf("%d", &array[i]);
+    }
+}
+void printArray(int*array, int size){
+    for(int i=0; i<size; ++i){
+        printf("array[%d]=%d\n",i,array[i]);
+    }
+}
+void fillRandom(int *array, int size){
+    srand(time(NULL));
+    for(int i=0; i<size; ++i){
+        array[i]=0+rand()%(size-0+1);
+    }
+}
+
